5f9ddc8001aca9d9d3e7af3b47a286244fb74fd6
CalebChen768 <72332844+CalebChen768@users.noreply.github.com>
2023-11-02 16:02:33 +0800

    Update process_addresses.py

--- .github/scripts/process_addresses.py
+++ .github/scripts/process_addresses.py
@@ -8,11 +8,13 @@ with open('data.txt', 'r') as file:
 ipv4_addresses = [item for item in data['payload'] if ':' not in item]
 ipv6_addresses = [item for item in data['payload'] if ':' in item]
 
+# Prepare the data for output
+ipv4_data = {'payload': ipv4_addresses}
+ipv6_data = {'payload': ipv6_addresses}
+
 # Write the addresses to separate files
 with open('ipv4.txt', 'w') as file:
-    for address in ipv4_addresses:
-        file.write(f"{address}\n")
+    yaml.dump(ipv4_data, file)
 
 with open('ipv6.txt', 'w') as file:
-    for address in ipv6_addresses:
-        file.write(f"{address}\n")
+    yaml.dump(ipv6_data, file)
